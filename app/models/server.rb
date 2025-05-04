18730af9d93f6291524969c5b3d77a3040fc6b41
KevinAlpizarDev <kevinalpizardev@gmail.com>
2025-05-04 10:38:42 -0600

    server and channel sidebar

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,4 +1,5 @@
 class Server < ApplicationRecord
   belongs_to :owner, class_name: "User"
-  has_many :channels, class_name: "Server::Channel", foreign_key: "server_id"
+  has_many :channels, class_name: "Server::Channel", foreign_key: "server_id", dependent: :destroy
+
 end
